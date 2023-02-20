f0e25847470c9d7b7af8beb59f47abc939812eb8
supercpq <shahzadashteyany016@gmail.com>
2023-02-20 20:26:34 +0800

    搜索框输入建议

--- src/components/searchLocal.vue
+++ src/components/searchLocal.vue
@@ -6,6 +6,9 @@
       :clearable="isclearable"
       class="input-with-select"
       @keyup.enter="hellogit(inputStore.inputSearch, select)"
+      @focus="setFocus(true)"
+      @blur="setFocus(false)"
+      @input="getSuggestion(inputStore.inputSearch)"
     >
       <template #prepend>
         <el-select
@@ -29,6 +32,34 @@
         />
       </template>
     </el-input>
+    <transition name="fade">
+      <ul
+        v-show="
+          inputSuggestion.length &&
+          autoComp &&
+          inputStore.inputSearch &&
+          isFocus
+        "
+        class="autoInput"
+      >
+        <li
+          class="autoIn"
+          v-for="(item, index) in inputSuggestion"
+          :key="index"
+          @click="
+            {
+              inputStore.inputSearch = item.label;
+              hellogit(inputStore.inputSearch, select);
+            }
+          "
+        >
+          <el-icon class="autoicon"><Search /></el-icon>
+          <span class="autolabel">
+            {{ item.label }}
+          </span>
+        </li>
+      </ul>
+    </transition>
   </div>
 </template>
 
@@ -37,14 +68,21 @@ import { ref, watch, onMounted, onBeforeUnmount } from "vue";
 import { searchInputStore } from "../store/modules/searchInputPinia";
 import _ from "lodash";
 import { Search } from "@element-plus/icons-vue";
+import axios from "axios";
 const props = defineProps({
   isSelf: { type: Boolean, required: true },
+  autoComp: { type: Boolean },
 });
+interface suggestion {
+  label: string;
+}
+const isFocus = ref(false);
 const inputSearch = ref("");
 const isclearable = true;
 const select = ref("https://www.google.com/search?q=");
-const value = ref("https://www.google.com/search?q=");
-const lable = ref("Google");
+// const value = ref("https://www.google.com/search?q=");
+// const lable = ref("Google");
+const inputSuggestion = ref<suggestion[]>([]);
 const options = [
   {
     value: "https://www.google.com/search?q=",
@@ -96,6 +134,30 @@ const hellogit = _.debounce(
     leading: true,
   }
 );
+function setFocus(focus: boolean) {
+  console.log(" setFocus", focus);
+  isFocus.value = focus;
+}
+const getSuggestion = _.debounce(
+  (input: string) => {
+    // bing的输入建议
+    axios.get(`/api/inputsuggestion?input=${input}`).then(
+      (res) => {
+        inputSuggestion.value.length = 0;
+        if (res.data.status === 0) {
+          inputSuggestion.value = res.data.suggest;
+        }
+      },
+      (err) => {
+        console.log(err.message);
+      }
+    );
+  },
+  200,
+  {
+    leading: true,
+  }
+);
 watch(select, (newV, oldV) => {
   localStorage.setItem("searchSelect", newV);
 });
@@ -123,5 +185,49 @@ onBeforeUnmount(() => {
 :deep(.input-with-select) {
   padding: auto;
   padding-top: 5px;
+}
+
+.autoInput {
+  z-index: 100;
+  position: absolute;
+  min-width: 300px;
+  max-width: 600px;
+  left: 30%;
+  background-color: rgba(255, 255, 255, 0);
+  padding: 5px;
+}
+
+.autoIn {
+  background-color: white;
+  background-color: rgba(255, 255, 255, 0.7);
+  text-align: left;
+  line-height: 30px;
+  display: flex;
+  gap: 0 10px;
+  align-items: center;
+}
+.autoIn:hover {
+  background-color: rgb(255, 255, 255);
+}
+
+.autolabel {
+  padding: 3px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  text-align: left;
+}
+
+.autoicon {
+  padding: 3px;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
